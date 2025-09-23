6cb446457a65e6ab43e252b55d93dbc6bc7288c4
Gabriel Holanda <holandagabriel880@gmail.com>
2025-09-22 23:34:47 -0300

    criando a função de renderizar tela

--- Front-End_Linketinder/dist/index.js
+++ Front-End_Linketinder/dist/index.js
@@ -1,5 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log('oi');
-console.log('davi e yag');
+function renderizarTela(tela) {
+    const pagina_selecionada = document.getElementById('container-principal');
+    if (!pagina_selecionada) {
+        throw new Error("Contêiner principal não encontrado.");
+    }
+    pagina_selecionada.innerHTML = '';
+    switch (tela) {
+        case 'cadastro-candidato':
+            pagina_selecionada.innerHTML = '<h1>Formulário de Cadastro de Candidato AQUI!</h1>';
+            break;
+        case 'cadastro-empresa':
+            pagina_selecionada.innerHTML = '<h1>Formulário de Cadastro de Empresa AQUI!</h1>';
+            break;
+    }
+}
 //# sourceMappingURL=index.js.map
--- Front-End_Linketinder/src/index.ts
+++ Front-End_Linketinder/src/index.ts
@@ -1,2 +1,21 @@
-console.log('oi')
-console.log('davi e yag')
+type nome_pagina = 'cadastro-candidato' | 'cadastro-empresa' | 'perfil-candidato' | 'perfil-empresa';
+
+function renderizarTela(tela: nome_pagina): void {
+  const pagina_selecionada = document.getElementById('container-principal');
+
+  if (!pagina_selecionada) {
+    throw new Error("Contêiner principal não encontrado.");
+  }
+
+  pagina_selecionada.innerHTML = '';
+
+  switch (tela) {
+    case 'cadastro-candidato':
+      pagina_selecionada.innerHTML = '<h1>Formulário de Cadastro de Candidato AQUI!</h1>';
+      break;
+    case 'cadastro-empresa':
+      pagina_selecionada.innerHTML = '<h1>Formulário de Cadastro de Empresa AQUI!</h1>';
+      break;
+  }
+}
+
